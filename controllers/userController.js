9ff9a1b249899998e7f0a3c550ae380763904811
Jared Puga Contreras <jaredpuga6@gmail.com>
2023-03-23 11:13:29 -0600

    Terminando modelo Usuarios

--- controllers/userController.js
+++ controllers/userController.js
@@ -128,7 +128,9 @@ const nuevoPassword = async (req, res) => {
 }
 
 const perfil = async(req, res) => {
-    console.log('desde perfil');
+    const { usuario } = req
+
+    res.json(usuario)
 }
 
 export {
--- middleware/checkAuth.js
+++ middleware/checkAuth.js
@@ -1,5 +1,29 @@
-const checkAuth = (req, res, next) => {
-    console.log('desde check .js');
+import  jwt  from "jsonwebtoken";
+import Usuario from "../models/usuario.js";
+
+const checkAuth = async(req, res, next) => {
+
+    let token
+
+    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
+        try {
+            token = req.headers.authorization.split(' ')[1]
+            console.log(token);
+
+            const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+            req.usuario = await Usuario.findById(decoded.id).select("-password -confirmado -token -createdAt -updatedAt -__v")
+
+            return next()
+        } catch (error) {
+            return res.status(404).json({mns: 'Hubo un error'})
+        }
+    }
+
+    if(!token) {
+        const error = new Error('Token no válido')
+        res.status(401).json({mns: error.message})
+    }
 
     next()
 }
