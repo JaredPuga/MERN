1da1e8a83edfa17c609f7338b6d7d71d7f5cb3e7
Jared Puga Contreras <jaredpuga6@gmail.com>
2023-04-06 13:00:27 -0600

    Tarea controllers

--- controllers/tareaController.js
+++ controllers/tareaController.js
@@ -1,21 +1,72 @@
+import mongoose from 'mongoose';
+import Proyecto from '../models/proyecto.js'
+import Tarea from '../models/tareas.js'
 
-const agregarTarea = async(req, res) = {
+const agregarTarea = async(req, res) => {
+    const {proyecto} = req.body
+
+    const valid = mongoose.Types.ObjectId.isValid(proyecto)
+
+    if (!valid) {
+        const error = new Error('Proyecto no encontrado');
+        return res.status(403).json({mns: error.message})
+    }
+
+    const existeProyecto = await Proyecto.findById(proyecto)
+
+    if(!existeProyecto) {
+        const error = new Error('Proyecto no encontrado');
+        return res.status(403).json({mns: error.message})
+    }
+
+    if(existeProyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('No tienes los permisos para hacer esta acción');
+        return res.status(403).json({mns: error.message})
+    }
+
+    try {
+        const tareaAlmacenada = await Tarea.create(req.body)
+        res.json(tareaAlmacenada)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+const obtenerTarea = async(req, res) => {
+    const { id } = req.params
+
+    const valid = mongoose.Types.ObjectId.isValid(id)
+
+    if (!valid) {
+        const error = new Error('La tarea no existe');
+        return res.status(403).json({mns: error.message})
+    }
+
+    const tarea = await Tarea.findById(id).populate("proyecto")
+     
+
+    if(!tarea) {
+        const error = new Error('No existe la tarea');
+        return res.status(404).json({mns: error.message})
+    }
+
+    if (tarea.proyecto.creador.toString() !== req.usuario._id.toString()) {
+        const error = new Error('Acción no válida');
+        return res.status(403).json({mns: error.message})
+    }
+
+    res.json(tarea)
+}
+
+const actualizarTarea = async(req, res) => {
 
 }
 
-const obtenerTarea = async(req, res) = {
+const eliminarTarea = async(req, res) => {
 
 }
 
-const actualizarTarea = (req, res) = {
-
-}
-
-const eliminarTarea = (req, res) = {
-
-}
-
-const cambiarEstado = (req, res) = {
+const cambiarEstado = async(req, res) => {
 
 }
 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import conectarDB from './config/db.js'
 import usuarioRoutes from "./routes/usuarioRoutes.js";
 import proyectosRoutes from './routes/proyectoRoutes.js'
+import tareasRoutes from './routes/tareaRoutes.js'
 
 const app = express();
 app.use(express.json()) //Para utilizar la respuesta json
@@ -14,7 +15,7 @@ conectarDB();
 //Routing
 app.use('/api/usuarios', usuarioRoutes)
 app.use('/api/proyectos', proyectosRoutes)
-
+app.use('/api/tareas', tareasRoutes)
 
 const PORT = process.env.PORT || 4000
 
